bd78d0555afde86146795c7f28e00af320204a0a
Niick_1607 <105996755+Niick1607@users.noreply.github.com>
2026-05-11 17:31:32 -0300

    alteracoes pagina editorPedidos

--- lib/firestoreTypes.ts
+++ lib/firestoreTypes.ts
@@ -8,3 +8,35 @@ export type Content = {
     id: string;
     [key: string]: any;
 }
+
+export interface Produto {
+    id: string;
+    nome: string;
+    marca: string;
+    modelo: string;
+    descricao: string | null;
+}
+
+export interface Vendedor {
+    uid: string;
+    nome: string;
+    perCent: number;
+}
+
+export interface VendedorTicket {
+    pedidoId: string | null;
+    produtoId: string | null;
+    produtoNome: string | null;
+    cliente: string | null;
+    uid: string;
+    nome: string;
+    perCent: number;
+}
+
+export interface Pedido {
+    id: string;
+    cliente: string;
+    data: string;
+    produtos: Produto[];
+    vendedores: Vendedor[];
+}
